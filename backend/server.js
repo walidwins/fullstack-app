b3c37342a2102d92f0903db97ea9f201ddc49478
walidwins <walidwins44@gmail.com>
2026-05-18 14:54:52 +0100

    Déploiement full-stack_TP8

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connecté"))
+.catch(err => console.log(err));
+
+app.get('/api/test', (req, res) => {
+    res.json({message: "API fonctionne"});
+});
+
+app.use(express.static(
+    path.join(__dirname, '../frontend/build')
+));
+
+app.get('/home', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, '../frontend/build/index.html')
+    );
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur ${PORT}`);
+});
