e0f0baedf6432aae32dffdd1186b8a109c076319
walidwins <walidwins44@gmail.com>
2026-05-21 18:44:27 +0100

    Premier deploy

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,22 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 app.get('/api/test', (req, res) => {
-    res.json({message: "API fonctionne"});
+    res.json({
+        message: "API fonctionne"
+    });
 });
+
+/*
+-----------------------------------
+Frontend React build
+-----------------------------------
+*/
 
 app.use(express.static(
     path.join(__dirname, '../frontend/build')
 ));
 
-app.get('/home', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(
         path.join(__dirname, '../frontend/build/index.html')
     );
@@ -30,5 +38,5 @@ app.get('/home', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur ${PORT}`);
+    console.log(`Serveur lancé sur le port ${PORT}`);
 });
